c7f1168333ad5363b7013548caaa71e8450da04a
Ashwans7 <ashwanshrestha.kun@gmail.com>
2025-06-18 00:59:58 +0545

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 let express = require("express")
 let app = express()
 
-app.get("/", (req,res) => {
-    res.send("<h1> This is homepage</h1>")
+//let ejs = require("ejs")
+app.set("view engine", "ejs")// express js ma chai js bhanne template engine as a frontend ko lagi use garna aateko xu tesko lagi k kati chaine configuration 
+//garne 
+
+
+app.get("/",(request,response)=> {
+    let name = "AShwan Shresstha"
+        //response.render("home.ejs")
+        response.render("home",{haha : name})
+
 })
 
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("this is contact")
+})
+
+app.use((req, res) => {
+    res.send("The route you are requesting is not found")
+})
 
 app.listen(3000,()=> {
     console.log("project has started at port 3000")
 })
+
+
